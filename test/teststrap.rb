2a0ac71d80b7bed0b39cf7e4cba1c5e9036052cc
Brian Carlson <brian.m.carlson@gmail.com>
2009-12-06 22:42:04 -0600

    Using dotmatrix reporter now that it reports failures and errors

--- test/teststrap.rb
+++ test/teststrap.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.dirname(__FILE__)+"/../lib")
 require 'riot-rack'
-#Riot.reporter = Riot::DotMatrixReporter
+Riot.reporter = Riot::DotMatrixReporter
 class OkayApp
   def self.call(env); [200,{"Content-Type"=>"text/html"},["okay!"]]; end
 end

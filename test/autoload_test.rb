3980b3d48331884e5ead8f382da5cb2b994bd941
Brian Carlson <brian.m.carlson@gmail.com>
2009-12-07 01:24:28 -0600

    changed some { } style blocks to do;end style blocks

--- test/autoload_test.rb
+++ test/autoload_test.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__)+"/teststrap.rb"
 context "Autoloading" do
 
   context "from the test directory" do
-    setup {
+    setup do
       Dir.chdir(File.dirname(__FILE__))
-    }
+    end
 
     get "/" do
       asserts('works') { last_response.body }.equals("yo!")
@@ -14,9 +14,9 @@ context "Autoloading" do
   end
 
   context "from the project root directory" do
-    setup {
+    setup do
       Dir.chdir(File.join(File.dirname(__FILE__)+"/.."))
-    }
+    end
     
     get "/" do
       should("work") { last_response.body }.equals('yo!')
